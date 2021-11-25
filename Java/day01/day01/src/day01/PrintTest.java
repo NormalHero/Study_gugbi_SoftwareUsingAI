c3454ca494bb088c0eb59989f9a03ccb9a71e834
NormalHero <mystparkkorea@gmail.com>
2021-11-25 16:02:26 +0900

    day01
    1일차 공부한 자료입니다 !

---
+++ Java/day01/day01/src/day01/PrintTest.java
@@ -0,0 +1,21 @@
+package day01;
+
+public class PrintTest {
+	public static void main(String[] args) {
+		// 주석
+		// 1. 소스코드에 설명글을 달 때
+		// 2. 지금 당장 사용하지 않는 코드를 번역하고 싶지않을 때 
+		
+		// 주석의 종류 
+		/*
+		 *1. 한줄 주석 : Ctrl + /
+		 *2. 범위 주석  : Ctrl + Shift + /, Ctrl + Shift + \
+		  */
+
+		//이름을 출력하는 부분
+		System.out.print("박민수");
+		// 나이를 출력하는 부분 
+		System.out.print("16살");
+
+	}
+}
