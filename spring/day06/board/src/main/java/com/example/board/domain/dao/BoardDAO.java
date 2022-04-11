ce17980786e6fe352a59b3b37510ba54a7ddf1ae
NormalHero <mystparkkorea@gmail.com>
2022-04-11 16:31:53 +0900

    day89
    89일차 공부한 자료입니다 !

---
+++ spring/day06/board/src/main/java/com/example/board/domain/dao/BoardDAO.java
@@ -0,0 +1,22 @@
+package com.example.board.domain.dao;
+
+import com.example.board.domain.vo.BoardVO;
+import com.example.board.domain.vo.Criteria;
+import com.example.board.mapper.BoardMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository  // 외부I/O 처리 :(퍼시스턴스 레이어, DB나 파일같은 외부 I/O 작업을 처리함)외부I/O 처리
+@RequiredArgsConstructor
+public class BoardDAO {
+    private final BoardMapper boardMapper;
+    // DAO 까지가 Persistence
+    public List<BoardVO> getList( Criteria criteria) {return boardMapper.getList(criteria);}
+    public void register(BoardVO boardVO) {boardMapper.insertSelectBno(boardVO);}
+    public boolean modify(BoardVO boardVO) {return boardMapper.update(boardVO) == 1;}
+    public boolean remove(Long bno) {return boardMapper.delete(bno) == 1;}
+    public BoardVO get(Long bno) {return boardMapper.get(bno);}
+    public int getTotal() {return boardMapper.getTotal();}
+}
---
+++ spring/day06/board/src/test/java/com/example/board/mapper/BoardMapperTests.java
@@ -0,0 +1,79 @@
+package com.example.board.mapper;
+
+import com.example.board.domain.vo.BoardVO;
+import com.example.board.domain.vo.Criteria;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+public class BoardMapperTests {
+
+    @Autowired
+    private  BoardMapper boardMapper;
+
+    @Test
+    public void testGetList(){
+        boardMapper.getList(new Criteria()).forEach(board ->log.info(board.toString()));
+    }
+    @Test
+    public void insert(){
+
+        BoardVO boardVO = new BoardVO();
+        boardVO.setContent("테스트 제목2");
+        boardVO.setTitle("테스트 내용2");
+        boardVO.setWriter("테스트작성자");
+        boardMapper.insert(boardVO);
+    }
+
+    @Test
+    public void testInsertSelectBno(){
+
+        BoardVO boardVO = new BoardVO();
+        boardVO.setContent("testInsertSelectBno 테스트");
+        boardVO.setTitle("testInsertSelectBno ");
+        boardVO.setWriter("작성자");
+        boardMapper.insertSelectBno(boardVO);
+
+        log.info("-----------------------------------------------");
+        log.info(boardVO.toString().toString());
+        log.info("-----------------------------------------------");
+    }
+
+    @Test
+    public void testUpdate(){
+        int result = 0;
+
+        BoardVO boardVO = new BoardVO();
+        boardVO.setContent("내용수정 완료");
+        boardVO.setTitle("내용수정 완료2");
+        boardVO.setBno(3L);
+        result = boardMapper.update(boardVO);
+        log.info("UPDATE COUNT : " + result);
+    }
+
+    @Test
+    public void testDelete(){
+        int result = 0;
+        result =  boardMapper.delete(5L);
+        log.info("Delete COUNT : " + result);
+    }
+
+    @Test
+    public  void testGet(){
+        int result = 0;
+        log.info(boardMapper.get(3L).toString());
+        log.info("Get COUNT : " + result);
+    }
+    @Test
+    public  void testGetTotal(){
+
+        log.info("전체 글의 개수"+boardMapper.getTotal());
+//        log.info(String.valueOf(boardMapper.getTotal()));
+
+    }
+
+
+}
